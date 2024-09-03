59fb324c9ef9708041ea6178ba7e6db552d4f9ec
Cleon <154812561+CleonDeSouza@users.noreply.github.com>
2024-09-03 19:11:48 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -12,24 +12,14 @@ function calculate(input){
     dsp.value = eval(dsp.value);
 }
 
+function trigSin(){
+    
+}
 
+function trigCos(){
+    
+}
 
+function trigTan(){
 
-
-
-
-
-
-/* BRAIN STORMING:
-b1 = 1(string format);
-b5 = 5(string format);
-+ concaternation = 5 + 1 = 51;
-+ type() turning into int format;
-51 = int;
-if any sign is pressed, then turn string into int;
-51 = string --> + pressed --> 51 = int
-
-
-if n1 + n2 = something, then n1 + n2 = ans, 
-if + other number then ans + n2 = ans(= output), 
-*/
+}
